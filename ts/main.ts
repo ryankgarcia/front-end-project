128fb19a16fa8d52b154fb7b4e19f12c0b85c56a
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-13 18:43:08 +0000

    Added styling adjustments, changed the h1 font to appear like the Figma, added placeholder values for the inputs.

--- ts/main.ts
+++ ts/main.ts
@@ -8,15 +8,15 @@ interface Events {
 
 async function fetchEventData(): Promise<void> {
   try {
-    // const response = await fetch(
-    //   'https://app.ticketmaster.com/discovery/v2/events.json?city=los+angeles&city=phoenix&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
-    // );
+    const response = await fetch(
+      'https://app.ticketmaster.com/discovery/v2/events.json?city=los+angeles&city=phoenix&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
+    );
     // const response = await fetch(
     //   'https://app.ticketmaster.com/discovery/v2/events.json?keyword=concert&city=los+angeles&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
     // );
-    const response = await fetch(
-      'https://app.ticketmaster.com/discovery/v2/events.json?includeSpellcheck=yes&keyword=concerta&page=2&city=los+angeles&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
-    );
+    // const response = await fetch(
+    //   'https://app.ticketmaster.com/discovery/v2/events.json?includeSpellcheck=yes&keyword=concerta&page=2&city=los+angeles&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
+    // );
 
     if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`);
 
