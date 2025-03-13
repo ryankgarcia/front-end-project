b83741ffb68dc5165007e6d9bc90c2057646a9eb
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-13 03:25:48 +0000

    Centered all the inputs of the app, added an overlay class to the background image, created an API call function to fetch for the data needed to populate the events, created an interface for the object data the app will be receiving, changed the font style of the h1 element.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-interface Events {
-  date: string;
-  name: string;
-  city: string;
-  venue: string;
-  startTime: string;
-}
-
-async function fetchEventData(): Promise<void> {
+'use strict';
+async function fetchEventData() {
   try {
     // const response = await fetch(
     //   'https://app.ticketmaster.com/discovery/v2/events.json?city=los+angeles&city=phoenix&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
@@ -17,14 +10,11 @@ async function fetchEventData(): Promise<void> {
     const response = await fetch(
       'https://app.ticketmaster.com/discovery/v2/events.json?includeSpellcheck=yes&keyword=concerta&page=2&city=los+angeles&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4',
     );
-
     if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`);
-
-    const data = (await response.json()) as Events;
+    const data = await response.json();
     console.log('Event Data:', data);
   } catch (error) {
     console.error('fetch failed!', error);
   }
 }
-
 fetchEventData();
