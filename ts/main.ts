a19fedac7d7030272a25f26f52c73b19bd3f9ea0
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-14 03:06:37 +0000

    Working on serializing only the data I need from the API to show the user.

--- ts/main.ts
+++ ts/main.ts
@@ -9,7 +9,11 @@ interface Events {
 // serialize the data to look like the interface above
 // the function i want to use is the map function
 // map function takes an array and returns a new array
-// to get the city look within the venues array
+// to get the city look within the venues array to track where it is so that
+// you can get to it
+
+// serialize using JSON.stringify method
+// deserialize using JSON.parse
 
 interface FormElements extends HTMLFormControlsCollection {
   location: HTMLInputElement;
@@ -31,10 +35,7 @@ if (!$mainPageView) throw new Error('$mainPageView query failed');
 if (!$itemsId) throw new Error('$itemsId query failed');
 if (!$results) throw new Error('$results query failed');
 
-async function fetchEventLocationData(
-  city: string,
-  keyword: string,
-): Promise<void> {
+async function fetchEventData(city: string, keyword: string): Promise<void> {
   try {
     const response = await fetch(
       `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&city=${city}&apikey=9tbiDGYZwnFcvD2p7AUJuB3lqwoWIfe4`,
@@ -43,9 +44,23 @@ async function fetchEventLocationData(
     if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`);
 
     const data = await response.json();
-    const events: Events[] = data?._embedded?.events;
+    // const events: Events[] = data?._embedded?.events;
     console.log('data:', data);
-    console.log('events', events);
+    // console.log('events', events);
+
+    // the code below is me trying to serialize the data like mentioned in the
+    // code near the top where the interface is
+    // i will be getting an array of objects and i want to create a new object
+    // with only the properties i want to show the user
+    const fetchedData = JSON.parse(data);
+    const newDataObj = fetchedData.map((obj: Events[]) => ({
+      date: obj,
+      name: obj,
+      city: obj,
+      venue: obj,
+      startTime: obj,
+    }));
+    console.log('newDataObj:', newDataObj);
     // for (let i = 0; i < data.length; i++) {
     //   const $li = renderEntry(data[i]);
     //   $itemsId.appendChild($li);
@@ -61,16 +76,16 @@ $form.addEventListener('submit', (event: Event) => {
   event.preventDefault();
   const $formElements = $form.elements as FormElements;
   if (!$formElements) throw new Error('$formElements query failed');
-  fetchEventLocationData(
-    $formElements.location.value,
-    $formElements.keyword.value,
-  );
+  fetchEventData($formElements.location.value, $formElements.keyword.value);
   // $form.reset();
 });
 
 // for the renderEntry function below only keep what you need in order to do
-// the function call correctly
+// the function call correctly. ask yourself what elements you want to make into
+// li elements and then append that to your DOM tree so that it updates for the user
+// dynamically
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function renderEntry(entry: any): HTMLLIElement {
   const $liRow = document.createElement('li');
   $liRow.setAttribute('class', 'row');
